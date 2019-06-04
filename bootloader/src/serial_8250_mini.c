5701f2ec6890b208347cbb173384630ddef0ff21
JaredWright <jared.wright12@gmail.com>
2019-06-04 20:19:44 +0000

    Configurable serial ports
    Added the ability to configure a base address for printing via a serial
    port on the Jetson TX1 and TX2.
    
    Signed-off-by: JaredWright <wrightj@ainfosec.com>

---
+++ bootloader/src/serial_8250_mini.c
@@ -0,0 +1,20 @@
+/*
+ * Miniture 8250 serial driver that prints a single character synchronously.
+ */
+
+#include <stdint.h>
+#include "bootloader_config.h"
+
+void _putc(char c)
+{
+    uintptr_t addr = BOOTLOADER_CONFIG_SERIAL_BASE;
+
+    asm volatile(
+      "1:  ldrb  w2, [%[addr], #20]\n"
+      "    tbz   w2, #5, 1b\n"
+      "    strb  %w[c], [%[addr]]"
+      :
+      : [c] "r"(c), [addr] "r"(addr)
+      : "x2"
+    );
+}
---
+++ scripts/cmake/config/jetson_tx1.cmake
@@ -0,0 +1,3 @@
+set(BUILD_IMAGE_FORMAT bin)
+set(DEVICE_TREE_SOURCE jetson-tx1-with-kernel-commandline.dts)
+set(SERIAL_BASE 0x70006000)
