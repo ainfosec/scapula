587ad1e34a388c69d9eac3ccb854c3b68362b565
JaredWright <jared.wright12@gmail.com>
2019-08-14 20:51:14 +0000

    Separate 32 bit vs 64 bit build artifacts
    1) Split up build steps for bootloader and test cases
    2) Build and package up 32/64 bit test cases seperately into an image tree
    
    Signed-off-by: Jared Wright <jared.wright12@gmail.com>

--- scripts/cmake/depends/libfdt.cmake
+++ scripts/cmake/depends/libfdt.cmake
@@ -1,59 +1,38 @@
-if(ENABLE_BUILD_SCAPULA AND NOT WIN32)
-    message(STATUS "Including dependency: libfdt")
+message(STATUS "Including dependency: libfdt")
 
-    set(LIBFDT_SOURCE_DIR ${CACHE_DIR}/dtc)
-    set(LIBFDT_BUILD_DIR ${DEPENDS_DIR}/libfdt/${VMM_PREFIX}/build)
-    set(LIBFDT_C_COMPILER /usr/bin/aarch64-linux-gnu-gcc)
+set(LIBFDT_SOURCE_DIR ${CACHE_DIR}/libfdt)
+set(LIBFDT_BUILD_DIR ${DEPENDS_DIR}/libfdt/${VMM_PREFIX}/build)
+set(LIBFDT_C_COMPILER aarch64-linux-gnu-gcc)
+set(LIBFDT_MAKEFILE_PATCH ${CMAKE_CURRENT_LIST_DIR}/PatchedLibfdtMakefile)
 
-    string(CONCAT LIBFDT_C_FLAGS
-        "-Wpointer-arith "
-        "-Wcast-qual "
-        "-Wnested-externs "
-        "-Wstrict-prototypes "
-        "-Wmissing-prototypes "
-        "-Wredundant-decls "
-        "-Wshadow "
-        "-Werror "
-        "-fno-stack-protector "
-        "-mgeneral-regs-only "
-        "-fno-common "
-        "-fno-builtin "
-        "-ffreestanding "
-        "-std=gnu99 "
-    )
+generate_flags(vmm NOWARNINGS)
 
-    if(${CMAKE_BUILD_TYPE} STREQUAL "Release")
-        string(CONCAT LIBFDT_C_FLAGS "-O3 ")
-    else()
-        string(CONCAT LIBFDT_C_FLAGS "-Os -g ")
-    endif()
+download_dependency(
+    libfdt
+    URL          ${DTC_URL}
+    URL_MD5      ${DTC_URL_MD5}
+)
 
-    download_dependency(
-        libfdt
-        URL          ${DTC_URL}
-        URL_MD5      ${DTC_URL_MD5}
-    )
-
-    add_dependency(
-        libfdt vmm
-        DEPENDS dtc_${VMM_PREFIX}
-        CONFIGURE_COMMAND   ${CMAKE_COMMAND} -E copy_directory ${LIBFDT_SOURCE_DIR} ${LIBFDT_BUILD_DIR}
-        BUILD_COMMAND
-            make -C ${LIBFDT_BUILD_DIR} libfdt
-            NO_PYTHON=1
-            CC=${LIBFDT_C_COMPILER}
-            CFLAGS=${LIBFDT_C_FLAGS}
-        INSTALL_COMMAND
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                ${LIBFDT_BUILD_DIR}/libfdt/libfdt.a ${VMM_PREFIX_PATH}/lib/libfdt.a
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                ${LIBFDT_BUILD_DIR}/libfdt/fdt.h ${VMM_PREFIX_PATH}/include/fdt.h
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                ${LIBFDT_BUILD_DIR}/libfdt/libfdt.h ${VMM_PREFIX_PATH}/include/libfdt.h
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                ${LIBFDT_BUILD_DIR}/libfdt/libfdt_env.h ${VMM_PREFIX_PATH}/include/libfdt_env.h
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                ${LIBFDT_BUILD_DIR}/libfdt/libfdt_internal.h ${VMM_PREFIX_PATH}/include/libfdt_internal.h
-    )
-
-endif()
+add_dependency(
+    libfdt vmm
+    DEPENDS dtc_${VMM_PREFIX}
+    CONFIGURE_COMMAND
+        COMMAND ${CMAKE_COMMAND} -E copy_directory ${LIBFDT_SOURCE_DIR} ${LIBFDT_BUILD_DIR}
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different ${LIBFDT_MAKEFILE_PATCH} ${LIBFDT_BUILD_DIR}/Makefile
+    BUILD_COMMAND
+        make -C ${LIBFDT_BUILD_DIR} libfdt
+        NO_PYTHON=1
+        CC=${LIBFDT_C_COMPILER}
+        USER_CFLAGS=${CMAKE_C_FLAGS}
+    INSTALL_COMMAND
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${LIBFDT_BUILD_DIR}/libfdt/libfdt.a ${VMM_PREFIX_PATH}/lib/libfdt.a
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${LIBFDT_BUILD_DIR}/libfdt/fdt.h ${VMM_PREFIX_PATH}/include/fdt.h
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${LIBFDT_BUILD_DIR}/libfdt/libfdt.h ${VMM_PREFIX_PATH}/include/libfdt.h
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${LIBFDT_BUILD_DIR}/libfdt/libfdt_env.h ${VMM_PREFIX_PATH}/include/libfdt_env.h
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${LIBFDT_BUILD_DIR}/libfdt/libfdt_internal.h ${VMM_PREFIX_PATH}/include/libfdt_internal.h
+)
--- scripts/cmake/targets.cmake
+++ scripts/cmake/targets.cmake
@@ -3,6 +3,7 @@ add_custom_target_category("Scapula Bootloader")
 add_custom_target(flash
     COMMAND mount ${FLASH_DEV} ${FLASH_MOUNT}
     COMMAND cp ${VMM_PREFIX_PATH}/boot/bootloader.bin ${FLASH_MOUNT}/${FLASH_PATH}
+    COMMAND cp ${VMM_PREFIX_PATH}/boot/device_tree.dtb ${FLASH_MOUNT}/${FLASH_PATH}/bfvmm.its
     COMMAND sync
     COMMAND umount ${FLASH_MOUNT}
     USES_TERMINAL
